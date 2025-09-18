9ff4c8da46012cd99a0750c6e004139f06bbbbb9
Mykola Marusenko <marusenkonik@gmail.com>
2025-09-18 18:32:39 +0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,11 +1,7 @@
 """Unit test for application."""
-from app import app
 
 
-def test_hello():
-
-    """Returns Hello, EDP!."""
-    response = app.test_client().get('/api/hello')
-
-    assert response.status_code == 200
-    assert response.data == b'Hello, EDP!'
+def test_example():
+    """Sample Test."""
+    assert True
+ 
